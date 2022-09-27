70958b6bb2ec029041d56e7240f6e0b14884a68d
adautomoises <adautomaleandro@gmail.com>
2022-09-27 12:00:01 -0300

    Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
+import { Car } from '../../components/Car';
+
 import {
   Container,
   Header,
@@ -13,6 +15,25 @@ import {
 } from './styles';
 
 export function Home(){
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao Dia',
+      price: 120,
+    },
+    thumbnail: 'https://www.webmotors.com.br/imagens/prod/348415/AUDI_RS5_2.9_V6_TFSI_GASOLINA_SPORTBACK_QUATTRO_STRONIC_34841510442727128.webp?s=fill&w=236&h=135&q=70&t=true'
+  }
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao Dia',
+      price: 340,
+    },
+    thumbnail: 'https://www.webmotors.com.br/imagens/prod/347468/PORSCHE_PANAMERA_2.9_V6_EHYBRID_4_PDK_34746806265508980.webp?s=fill&w=440&h=330&q=80&t=true'
+  }
+
   return (
     <Container>
       <StatusBar 
@@ -31,6 +52,10 @@ export function Home(){
           </TotalCars>
          </HeaderContent>
       </Header>
+
+      <Car data={carDataOne}/>
+      <Car data={carDataTwo}/>
+
     </Container>
   );
 }
