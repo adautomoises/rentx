acf8d1af108a561021155f59a5c9c1b5e8d023f7
adautomoises <adautomaleandro@gmail.com>
2022-10-02 17:00:48 -0300

    Exibindo Carros Agendados

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,14 +1,39 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { FlatList, StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import { Car } from '../../components/Car';
 import { CarDTO } from '../../dtos/CarDTO';
 import api from '../../services/api';
 
 import {
-  Container
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
 } from './styles';
 
+interface CarProps {
+  user_id: string;
+  id: string;
+  car: CarDTO;
+}
+
 export function MyCars(){
-  const [ cars, setCars ] = React.useState<CarDTO>({} as CarDTO);
+  const [ cars, setCars ] = React.useState<CarProps[]>([]);
   const [ loading, setLoading ] = React.useState(true);
+
+  const navigation = useNavigation();
+  const theme = useTheme();
+
+  function handleBack(){
+    navigation.goBack();
+  };
 
   React.useEffect(() => {
     async function fetchCars(){
@@ -26,7 +51,42 @@ export function MyCars(){
 
   return (
     <Container>
+      <Header>
+        <StatusBar 
+          barStyle="light-content"
+          translucent
+          backgroundColor= "transparent"
+        />
+        <BackButton
+          onPress={handleBack}
+          color={theme.colors.shape}
+        />
 
+        <Title>
+          Seus agendamentos,{'\n'}
+          estão aqui.
+        </Title>
+
+        <SubTitle>
+          Conforto, segurança e praticidade.
+        </SubTitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>05</AppointmentsQuantity>
+        </Appointments>
+
+        <FlatList 
+          data={cars}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <Car data={item.car} />
+          )}
+        />
+      </Content>
     </Container>
   );
 }
