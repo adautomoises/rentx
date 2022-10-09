fceea7b55e4b4795ee926497aeb567dceb24a28a
adautomoises <adautomaleandro@gmail.com>
2022-10-08 22:40:08 -0300

    Nova customização para o componente Button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -6,17 +6,22 @@ interface ButtonProps extends RectButtonProps {
   color: string;
 }
 
+interface ButtonTextProps {
+  light: boolean;
+}
+
 export const Container = styled(RectButton)<ButtonProps>`
   width: 100%;
 
   padding: 19px;
   align-items: center;
   justify-content: center;
-
+  margin-bottom: 8px;
   background-color: ${({ color }) => color};
 `;
-export const Title = styled.Text`
+export const Title = styled.Text<ButtonTextProps>`
   font-family: ${({ theme }) => theme.fonts.primary_500};
-  color: ${({ theme }) => theme.colors.shape};
   font-size: ${RFValue(15)}px;
+  color: ${({ theme, light }) => light ?
+   theme.colors.header : theme.colors.shape};
 `;
